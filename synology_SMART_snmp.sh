97f41a177e9612ff705bde01b827138263bed039
wallacebrf <62187108+wallacebrf@users.noreply.github.com>
2024-11-05 10:01:07 -0600

    Update synology_SMART_snmp.sh
    !!!NOTICE!!! !!!NOTICE!!! !!!NOTICE!!! This script has now been replaced with a MUCH simplier but also MUCH faster script using "smartctl" instead of SNMP.
    
    It can be found here: https://github.com/wallacebrf/SMART-to-InfluxDB-Logger
    
    This script using SNMP took 1 minute and 22 seconds to execute on a Synology NAS with 9x HDDs and no NVME drives. The new version using smartctl takes seconds.

--- synology_SMART_snmp.sh
+++ synology_SMART_snmp.sh
@@ -2,6 +2,23 @@
 #version 4.5 dated 9/26/2023
 #By Brian Wallace
 
+
+
+!!!NOTICE!!! !!!NOTICE!!! !!!NOTICE!!! This script has now been replaced with a MUCH simplier but also MUCH faster script using "smartctl" instead of SNMP.
+
+It can be found here: https://github.com/wallacebrf/SMART-to-InfluxDB-Logger
+
+This script using SNMP took 1 minute and 22 seconds to execute on a Synology NAS with 9x HDDs and no NVME drives. The new version using smartctl takes seconds.
+
+
+
+
+
+
+
+
+
+
 #This script pulls various information from the Synology NAS
 #add this script to a scheduled task within Synology task scheduler
 
